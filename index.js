89ee5974dc24c160324bb21a5c377ca9f7c298c5
zakaria ziouziou <zakaria.Ziouziou@metropolia.fi>
2019-11-07 13:23:41 +0200

    express app, get / and get /demo

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 'use strict';
 
+console.log('my powerful app.');
+
+const express = require('express');
+
+const port = 3000;
+const app = express();
+
+
+app.get('/', (req, res) => {
+    res.send('Hello from my node server');
+});
+
+app.get('/demo', (req, res)=> {
+	
+		console.log('request', req);
+		res.send('demo');
+});
+
+app.listen(3000, () => {
+	console.log('server app start?');
+});
