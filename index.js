1ddb6ac7997c22374d416c0c8e2640cfe5c3c72a
zakaria ziouziou <zakaria.Ziouziou@metropolia.fi>
2019-11-15 12:57:11 +0200

    serving static files

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const express = require('express');
 const port = 3000;
 const app = express();
 
+app.use(express.static('public'));
+
 
 app.get('/', (req, res) => {
     res.send('Hello from my node server');
